f211b3762f34f29583a134a0ce289896a026364d
Agustina Edmé Aliciardi <agustina.aliciardi@claro.com.ar>
2022-03-24 15:46:36 -0300

    Add tests for exercise 2 & 3

--- README.md
+++ README.md
@@ -2,18 +2,23 @@
 Código destinado a autocorregir el código desarrollado por los estudiantes en la materia "Laboratorio de Computación 2".
 El código se corresponde con las unidades:
 
-Trabajo práctico N°1: Instalación y Características de los SO
+Trabajo práctico N°1: Características Básicas de los SO
+
+[x] Ejercicio 1: Primera Página web
 
 Trabajo práctico N°2: Drivers
 
-Trabajo práctico N°3: Herramientas
+Trabajo práctico N°3: Herramientas de Desarrollo
 
 Trabajo práctico N°4: Prototipado
 
-Trabajo práctico N°5: Análisis teórico de la interfaz de usuario
+Trabajo práctico N°5: Aplicaciones Web
 
-Trabajo práctico N°6: Aplicaciones Web
+Trabajo práctico N°6: Orientación a Eventos
 
-Trabajo práctico N°7: Orientación a Eventos
+Trabajo práctico N°7: Aplicaciones Móviles
 
-Trabajo práctico N°8: Aplicaciones Móviles
+## Modo de instalación
+`npm install`
+## Modo de ejecución
+`npm run cypress:open`
--- cypress/integration/tests/Exercises-html.spec.js
+++ cypress/integration/tests/Exercises-html.spec.js
@@ -1,19 +1,20 @@
 /**
  * @author Agustina Edmé Aliciardi
  */
-import githubPage from '../pages/githubPage';
-import data from '../../fixtures/students.json'
-
-const gh = new githubPage;
-var stuId = 0;
-
-describe('Ejercicios de HTML', () => {
-
-    before(() => {
+ import githubPage from '../pages/githubPage';
+ import data from '../../fixtures/students.json'
+ 
+ const gh = new githubPage;
+ var stuId = 0;
+ 
+ describe('Ejercicios de HTML', () => {
+ 
+    beforeEach(() => {
         cy.visit(data[stuId].github_exercises);
     });
 
     it('Ejercicio 1: Primera Página web', () => {
+        cy.log('https://ucc-labcompu2.github.io/filminas/U4_HTML.html#/9');
         let repositoryName = data[stuId].github_exercises.split('/');
         repositoryName = repositoryName[repositoryName.length-1]
 
@@ -22,5 +23,59 @@ describe('Ejercicios de HTML', () => {
         gh.getBreadcrum().should('contain.text', repositoryName+'/Ejercicios-HTML/');
         cy.get('a').contains('index.html').click();
         gh.getLineCode().contains('<!DOCTYPE html>');
+        gh.getLineCode().contains('<head>');
+        gh.getLineCode().contains('<title>');
+        gh.getLineCode().contains('</title>');
+        gh.getLineCode().contains('name="autor"').contains('meta');
+        gh.getLineCode().contains('name="descripcion"').contains('meta');
+        gh.getLineCode().contains('name="keywords"').contains('meta');
     });
-})
+
+    it('Ejercicio 2: Secciones', () => {
+        cy.log('https://ucc-labcompu2.github.io/filminas/U4_HTML.html#/17');
+        let repositoryName = data[stuId].github_exercises.split('/');
+        repositoryName = repositoryName[repositoryName.length-1]
+
+        gh.getHTMLfolder().click();
+        cy.wait(200);
+        gh.getBreadcrum().should('contain.text', repositoryName+'/Ejercicios-HTML/');
+        cy.get('a').contains('ej_noticia.html').click();
+        //Header
+        gh.getLineCode().contains('<header>');
+        gh.getLineCode().contains('</header>');
+        gh.getLineCode().contains('<img src="imagenes/');
+        //Section
+        gh.getLineCode().contains('<section>');
+        gh.getLineCode().contains('</section>');
+        gh.getLineCode().contains('<p>');
+        gh.getLineCode().contains('</p>');
+        gh.getLineCode().contains('<u>');
+        gh.getLineCode().contains('</u>');
+        gh.getLineCode().contains('<b>');
+        gh.getLineCode().contains('</b>');
+        gh.getLineCode().contains('<i>');
+        gh.getLineCode().contains('</i>');
+        //Footer
+        gh.getLineCode().contains('<footer>');
+        gh.getLineCode().contains('</footer>');
+    });
+
+    it('Ejercicio 3: Títulos', () => {
+        cy.log('https://ucc-labcompu2.github.io/filminas/U4_HTML.html#/18');
+        let repositoryName = data[stuId].github_exercises.split('/');
+        repositoryName = repositoryName[repositoryName.length-1]
+
+        gh.getHTMLfolder().click();
+        cy.wait(200);
+        gh.getBreadcrum().should('contain.text', repositoryName+'/Ejercicios-HTML/');
+        cy.get('a').contains('ej_noticia.html').click();
+        gh.getLineCode().contains('<h1>');
+        gh.getLineCode().contains('</h1>');
+        gh.getLineCode().contains('<h3>');
+        gh.getLineCode().contains('</h3>');
+        gh.getLineCode().contains('<h6>');
+        gh.getLineCode().contains('</h6>');
+        gh.getLineCode().contains('<h5>');
+        gh.getLineCode().contains('</h5>');
+    });
+ })
